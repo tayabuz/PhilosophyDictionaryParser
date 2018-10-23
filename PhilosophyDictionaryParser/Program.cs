9c5bb6ac6015d7da3a7ebfa0c4cf315e113c0d76
tayabuz <taya.buzhinskaya@gmail.com>
2018-10-23 22:16:37 +0500

    Добавьте файлы проекта.

---
+++ PhilosophyDictionaryParser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhilosophyDictionaryParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = TextFileParser.DictionaryPhilosophy;
+            int k = 0;
+        }
+    }
+}
---
+++ PhilosophyDictionaryParser/TextFileParser.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PhilosophyDictionaryParser
+{
+    public class TextFileParser
+    {
+        public static Dictionary<string, string> DictionaryPhilosophy { get; set; } = new Dictionary<string, string>();
+        private string pathToFile = @"DictionaryResourse.txt";
+
+        public TextFileParser()
+        {
+            ParseDictionaryContent();
+        }
+        private string[] DictionaryContent()
+        {
+            string[] textInFile = File.ReadAllLines(pathToFile);
+            return textInFile;
+        }
+
+        private void ParseDictionaryContent()
+        {
+            string[] dictContent = DictionaryContent();
+            string Definition = "";
+            string Explanation = "";
+            foreach (var newLine in dictContent)
+            {
+                if (newLine.Contains(" "))
+                {
+                    var firstWord = newLine.Substring(0, newLine.IndexOf(" "));
+                    if (firstWord.All(char.IsUpper) && firstWord.Length > 1)
+                    {
+                        Definition = newLine.Substring(0, newLine.IndexOf("—")).Trim();
+                        Explanation = newLine.Substring(newLine.IndexOf("—") + 2, newLine.Length - 1 - (newLine.IndexOf("—") + 2)).Trim();
+                    }
+                    else { Explanation = Explanation + newLine; }
+                }
+                else { Explanation = Explanation + newLine; }
+                if (DictionaryPhilosophy.ContainsKey(Definition)) { DictionaryPhilosophy[Definition] = Explanation; }
+                else { DictionaryPhilosophy.Add(Definition, Explanation); }
+            }
+        }
+    }
+}
